8f20fd51c0759adce4cb46268bd2f541f3eff1dd
Jason Bacon <bacon@coral.acadix.biz>
2021-03-23 10:45:55 -0500

    Add man page and test data

---
+++ Test/run-test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+##########################################################################
+#   This test utilizes randomly generated VCF data and haplotype output
+#   generated at a time when vcf2hap was known to be working properly.
+##########################################################################
+
+../vcf2hap --xz generand < generand.vcf.xz > test-results.hap
+if diff -u generand-correct.hap test-results.hap; then
+    printf "Test passed.\n"
+else
+    printf "Differences found, something is wrong.\n"
+fi
